7151088af2fb59ce768428f92ef9fa704ed00b88
Nate Micinski <nmicinski@gmail.com>
2019-04-24 21:31:11 -0700

    add jumbotron-fluid

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 function Jumbotron() {
     return (
-      <div className="jumbotron text-center mt-5">
+      <div className="jumbotron jumbotron-fluid text-center mt-5">
         <h1 className="display-4">Clicky Game!</h1>
         <p className="lead">
           Click on an image to earn points, but don't click on any more than
