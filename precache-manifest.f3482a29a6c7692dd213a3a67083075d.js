5605cdbe4695c7294b565bf163162254a0ab3f24
Nate Micinski <nmicinski@gmail.com>
2019-04-26 19:48:02 -0700

    Updates

--- precache-manifest.f3482a29a6c7692dd213a3a67083075d.js
+++ precache-manifest.f3482a29a6c7692dd213a3a67083075d.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
   },
   {
-    "revision": "18f716cbc1ea8dafd692",
-    "url": "/clicky-game/static/js/main.2ea52178.chunk.js"
+    "revision": "ba3ac9099132291dcb99",
+    "url": "/clicky-game/static/js/main.c3b3a764.chunk.js"
   },
   {
     "revision": "dd0ed99a380f8d225736",
     "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
   },
   {
-    "revision": "18f716cbc1ea8dafd692",
+    "revision": "ba3ac9099132291dcb99",
     "url": "/clicky-game/static/css/main.a456a88a.chunk.css"
   },
   {
-    "revision": "4158f26f3b9781e16fffd7f7cd788884",
+    "revision": "2d498c5cc7f46d8e8754fe7082dfd885",
     "url": "/clicky-game/index.html"
   }
 ];
